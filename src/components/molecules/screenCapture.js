46a0f7568534b15b3b0f8f582815d70be7a224ed
DESKTOP-GUJB72J\Tyler <Tyler.martinez89@gmail.com>
2020-06-10 15:21:59 -0400

    Finish basic screen capture functionality

--- src/components/molecules/screenCapture.js
+++ src/components/molecules/screenCapture.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import styled from 'styled-components'
 //import PropTypes from 'prop-types'
 import { Button } from '../atoms'
+import { remote } from 'electron'
+import { takeScreenshot } from '../../utils/capturingUtils'
 
 
 const ScreenCaptureStyle = styled.div`
@@ -10,9 +12,15 @@ const ScreenCaptureStyle = styled.div`
 
   .view {
     background: black;
-    margin: 15px;
+    margin-top: 15px;
     position: relative;
     height: calc(100% - 30px);
+  }
+
+  .viewInner {
+    height: 100%;
+    width: 100%;
+    text-align: center;
   }
   
   .setBoundsButton {
@@ -20,19 +28,79 @@ const ScreenCaptureStyle = styled.div`
     bottom: 5px;
     right: 5px;
   }
+
+  .image{
+    vertical-align: middle;
+    max-height: 100%;
+    max-width: 100%;
+  }
+
+  .imageHelper{
+    display: inline-block;
+    height: 100%;
+    vertical-align: middle;
+  }
 `
 
 const ScreenCapture = () => {
+  // Ref Hooks
+  var boundsWindow = useRef(null)
+
   // State Hooks
+  var [boundsWindowOpen, setBoundsWindowOpen]  = useState(false)
+  var [screenShot, setScreenShot]  = useState(null)
 
   // Functions
+  const toggleBoundsWindow = async () => {
+    if(boundsWindow.current){
+      // Get window
+      let window = await boundsWindow.current.webContents.executeJavaScript('window')
+
+      // Get Crop
+      let crop = {
+        height: window.innerHeight - 27,
+        width: window.innerWidth - 4,
+        distanceX: window.screenLeft ,
+        distanceY: window.screenTop - 1
+      }
+
+      await takeScreenshot(boundsWindow.current.getBounds(), crop, (img) => {setScreenShot(img)})
+
+      boundsWindow.current.close()
+      boundsWindow.current = null;
+      setBoundsWindowOpen(false)
+    } else {
+      // Create window
+      boundsWindow.current = new remote.BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: false,
+        minWidth: 100,
+        minHeight: 100,
+        transparent: true
+      })
+
+      // and load the index.html of the app.
+      boundsWindow.current.loadFile('./captureIndex.html')
+
+      // Emitted when the window is closed.
+      boundsWindow.current.on('closed', () => {
+        boundsWindow.current = null
+      })
+
+      setBoundsWindowOpen(true)
+    }
+  }
 
   // Render
   return (
     <ScreenCaptureStyle className="flex-grow-1">
       <div className="view">
-        <Button className="setBoundsButton">
-          Set Bounds
+        {screenShot &&
+          <div className="viewInner"><span className="imageHelper"></span><img className="image" src={screenShot}></img></div>
+        }
+        <Button className="setBoundsButton" onClick={() => toggleBoundsWindow()}>
+          {boundsWindowOpen ? "Capture Bounds" : "Set Bounds"}
         </Button>
       </div>
     </ScreenCaptureStyle>
---
+++ src/utils/capturingUtils.js
@@ -0,0 +1,97 @@
+import { desktopCapturer, remote } from 'electron'
+
+const handleStream = (stream, crop, callback) => {
+  // Create video tag
+  var video = document.createElement('video');
+
+  // Hide it
+  video.style.cssText = 'display:none;';
+  
+  // Onloaded event
+  video.onloadedmetadata = function () {
+      // Set original height
+      video.style.height = this.videoHeight + 'px'; // videoHeight
+      video.style.width = this.videoWidth + 'px'; // videoWidth
+
+      // Capture
+      video.play();
+
+      // Create canvas
+      var canvas = document.createElement('canvas');
+
+      // Set Height
+      canvas.width = crop.width;
+      canvas.height = crop.height;
+
+      // Hide it
+      canvas.style.cssText = 'display:none;';
+
+      var ctx = canvas.getContext('2d');
+
+      // Draw video on canvas
+      ctx.drawImage(video, crop.distanceX, crop.distanceY, crop.width, crop.height, 0, 0, crop.width, crop.height);
+      //ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+      if (callback) {
+          // Save screenshot to base64
+          callback(canvas.toDataURL('image/jpeg'));
+      }
+
+      // Remove tags
+      video.remove();
+      canvas.remove();
+
+      // Clean up
+      try {
+          // Destroy connect to stream
+          stream.getTracks()[0].stop();
+      } catch (e) {
+        console.warn("Failed to clean up stream!")
+      }
+  }
+  
+  video.srcObject = stream;
+  document.body.appendChild(video);
+};
+
+const handleError = (e) => {
+  console.log(e);
+};
+
+export const takeScreenshot = async (bounds, crop, callback) => {
+  // Determine screen
+  let screenSelected = remote.screen.getDisplayNearestPoint({x: bounds.x, y: bounds.y})
+
+  // Adjust crop for multiple displays
+  crop.distanceX = Math.abs(screenSelected.bounds.x - crop.distanceX) + 2
+  crop.distanceY = Math.abs(screenSelected.bounds.y - crop.distanceY) + 26
+
+  // Get Sources
+  let sources = await desktopCapturer.getSources({ types: ['window', 'screen'] })
+  console.log(sources);
+  
+  // Select correct source and set up stream
+  for (const source of sources) {
+      if (source.display_id === screenSelected.id.toString()) {
+          try{
+              const stream = await navigator.mediaDevices.getUserMedia({
+                  audio: false,
+                  video: {
+                      mandatory: {
+                          chromeMediaSource: 'desktop',
+                          chromeMediaSourceId: source.id,
+                          minWidth: 0,
+                          maxWidth: 4000,
+                          minHeight: 0,
+                          maxHeight: 4000
+                      }
+                  }
+              });
+
+              handleStream(stream, crop, callback);
+          } catch (e) {
+              handleError(e);
+          }
+      }
+  }
+}
